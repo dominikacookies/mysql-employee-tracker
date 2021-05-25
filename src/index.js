14d984a4610403ff9d98b30a7964341de78bc05a
Dominika <domi_nika@hotmail.co.uk>
2021-05-25 12:20:52 +0100

    prompts inquirer progress options questions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log("hello")
+const inquirer = require("inquirer");
+
+const Database = require("./db/Database");
+
+const startApp = async () => {
+  // const db = new Database("company_db");
+
+  // db.start();
+
+  let inProgress = true;
+
+  while (inProgress) {
+    const progressQuestions = {
+      type: "list",
+      message: "What would you like to do?",
+      name: "progressChoice",
+      choices: [
+        {
+          short: "View",
+          value: "VIEW",
+          name: "View information"
+        },
+        {
+          short: "Update",
+          value: "UPDATE",
+          name: "Update information"
+        },
+        {
+          short: "Delete",
+          value: "DELETE",
+          name: "Delete information"
+        },
+        {
+          short: "Exit",
+          value: "EXIT",
+          name: "Exit the application"
+        },
+      ]
+    }
+    const { progressChoice } = await inquirer.prompt(progressQuestions);
+    console.log(progressChoice)
+  }
+  
+
+}
+
+startApp ()
